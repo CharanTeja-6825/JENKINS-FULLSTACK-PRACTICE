5c75ad4429c16596642923dab1e56f6900327822
CharanTeja <rcharanteja2006@gmail.com>
2025-09-23 12:13:57 +0530

    modified navbar

--- PRODUCT-FRONTEND/src/components/Navbar.jsx
+++ PRODUCT-FRONTEND/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     <div>
         <BrowserRouter>
             <nav className='flex gap-5 justify-center mt-5'>
-                <Link className='bg-gray-300 p-2 rounded-md hover:bg-gray-400' to="/add">Add Product</Link>
+                <Link className='bg-gray-300 p-2 rounded-md hover:bg-gray-400' to="/">Add Product</Link>
                 <Link className='bg-gray-300 p-2 rounded-md hover:bg-gray-400' to="/manage">Manage</Link>
                 <Link className='bg-gray-300 p-2 rounded-md hover:bg-gray-400' to="/find">Find Product</Link>
             </nav>
